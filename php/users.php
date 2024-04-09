a0c1857bb59e32ea90bb4901730788c67aa1d7c7
Awizz <ruben.penalvez@gmail.com>
2024-04-09 09:50:25 +0200

    Redirection vers accueil.php si l'utilisateur essaye d'accéder a la page Admin

--- php/users.php
+++ php/users.php
@@ -1,3 +1,11 @@
+<?php session_start();
+?>
+<?php if ((isset($_SESSION["Loggedin"]) === false) || (($_SESSION['Id']) === 100)) {
+  header("Location: ../php/accueil.php");
+  exit;
+}
+?>
+
 <?php
 
 require_once "./DB_Conn.php";
@@ -18,29 +26,29 @@ foreach ($resultat as $resultats) {
       <?php echo $resultats['A_Mail'] ?>
       <?php echo $resultats['A_Mdp'] ?>
     </h5>
-        <!-- Formulaire pour modifier les informations de l'utilisateur -->
-        <form method="post" action="update_user.php">
-        <input type="hidden" name="user_id" value="<?php echo $resultats['A_Id'] ?>">
-        <input type="text" name="new_username" placeholder="Nouveau nom d'utilisateur">
-        <input type="email" name="new_email" placeholder="Nouvelle adresse e-mail">
-        <input type="password" name="new_password" placeholder="Nouveau mot de passe">
-        <button type="submit">Modifier</button>
+    <!-- Formulaire pour modifier les informations de l'utilisateur -->
+    <form method="post" action="update_user.php">
+      <input type="hidden" name="user_id" value="<?php echo $resultats['A_Id'] ?>">
+      <input type="text" name="new_username" placeholder="Nouveau nom d'utilisateur">
+      <input type="email" name="new_email" placeholder="Nouvelle adresse e-mail">
+      <input type="password" name="new_password" placeholder="Nouveau mot de passe">
+      <button type="submit">Modifier</button>
     </form>
     <!-- Formulaire pour supprimer l'utilisateur -->
     <form method="post" action="delete_user.php">
-        <input type="hidden" name="user_id" value="<?php echo $resultats['A_Id'] ?>">
-        <button type="submit">Supprimer</button>
+      <input type="hidden" name="user_id" value="<?php echo $resultats['A_Id'] ?>">
+      <button type="submit">Supprimer</button>
     </form>
-</div>
-<?php
+  </div>
+  <?php
 }
 ?>
 
 <!-- Formulaire pour ajouter un nouvel utilisateur -->
 <form method="post" action="add_user.php">
-    <input type="text" name="username" placeholder="Nom d'utilisateur">
-    <input type="email" name="email" placeholder="Adresse e-mail">
-    <input type="password" name="password" placeholder="Mot de passe">
-    <button type="submit">Ajouter utilisateur</button>
+  <input type="text" name="username" placeholder="Nom d'utilisateur">
+  <input type="email" name="email" placeholder="Adresse e-mail">
+  <input type="password" name="password" placeholder="Mot de passe">
+  <button type="submit">Ajouter utilisateur</button>
 </form>
-  </div>
+</div>
